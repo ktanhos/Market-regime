19c3ba6e995c47663fa13891f14525906d344293
ktanhos <khayductan66@gmail.com>
2026-08-21 10:28:39 +0700

    Add VN30 update universe

---
+++ src/universe.py
@@ -0,0 +1,5 @@
+VN30_SYMBOLS = [
+    "ACB","BCM","BID","BVH","CTG","FPT","GAS","GVR","HDB","HPG",
+    "LPB","MBB","MSN","MWG","PLX","SAB","SHB","SSB","SSI","STB",
+    "TCB","TPB","VCB","VHM","VIB","VIC","VJC","VNM","VPB","VRE"
+]
